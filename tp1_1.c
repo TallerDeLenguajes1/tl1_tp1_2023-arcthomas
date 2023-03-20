37a7b12807b0fee6e40ac4a6b677fec0961de9a0
Thomas Bertoluzzi <bertoluzzit2@gmail.com>
2023-03-19 23:39:12 -0300

    Se añadió una variable y un puntero, sus contenidos y direcciones de memoria

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     printf("Hola mundo!");
+    int a = 2;
+    int *pA = &a;
+    printf("\nContenido del puntero: %d", *pA);
+    printf("\nDirección de memoria almacenada por el puntero: %d", pA);
+    printf("\nDirección de memoria de la variable: %d", &a);
+    printf("\nDirección de memoria del puntero: %d", &pA);
+    printf("\nTamaño de memoria utilizado por la variable (sizeof): %d", sizeof(a));
 }
