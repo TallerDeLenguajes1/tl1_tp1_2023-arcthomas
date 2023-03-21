427555dadbcf21c15b1d57ef4bbbe4de05bc9889
Thomas Bertoluzzi <bertoluzzit2@gmail.com>
2023-03-21 11:56:57 -0300

    Desarrollo 4.c

--- tp1_2.c
+++ tp1_2.c
@@ -11,9 +11,17 @@ void cuadradoVoid(int num)
     printf("El cuadrado del numero: %d\n", num * num);
 }
 
+void mostrarContDir(int *var)
+{
+    printf("Dirección de memoria de la variable: %d\n", var);
+    printf("Contenido de la variable: %d\n", *var);
+}
+
 int main()
 {
-    int a = 4;
+    int a = 5;
+    int *pA = &a;
     printf("El cuadrado del numero: %d\n", devolverCuadrado(a));
     cuadradoVoid(a);
+    mostrarContDir(pA);
 }
